2ca0637c65c142aa07ab1a73af4d079248e6f43d
Abetar <alexisgomez_96@hotmail.com>
2025-05-22 12:20:51 -0600

    init traductor clinico

--- next.config.ts
+++ next.config.ts
@@ -5,4 +5,10 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
 };
 
+module.exports = {
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
+};
+
 export default nextConfig;
